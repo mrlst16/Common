1d2e33a859f531315dd74982a68dd78bad3999ab
matt <mrlst16@mail.rmu.edu>
2022-09-12 15:15:21 -0700

    make mapper syncronous

--- Common.Interfaces/Utilities/IMapper.cs
+++ Common.Interfaces/Utilities/IMapper.cs
@@ -2,16 +2,16 @@
 {
     public interface IMapper<TIn, TOut>
     {
-        Task<TOut> Map(TIn source);
+        TOut Map(TIn source);
     }
 
     public interface IMapper<TIn1, TIn2, TOut>
     {
-        Task<TOut> Map(TIn1 source1, TIn2 source2);
+        TOut Map(TIn1 source1, TIn2 source2);
     }
 
     public interface IMapper<TIn1, TIn2, TIn3, TOut>
     {
-        Task<TOut> Map(TIn1 source1, TIn2 source2, TIn3 source3);
+        TOut Map(TIn1 source1, TIn2 source2, TIn3 source3);
     }
 }
