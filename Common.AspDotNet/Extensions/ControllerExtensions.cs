086cb6b5948b6bba517abe7d6be495a3f206cd3f
matt <mrlst16@mail.rmu.edu>
2022-11-29 20:17:21 -0800

    modify wrapper function call

--- Common.AspDotNet/Extensions/ControllerExtensions.cs
+++ Common.AspDotNet/Extensions/ControllerExtensions.cs
@@ -1,20 +1,26 @@
 ﻿using Common.Models.Responses;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Common.AspDotNet.Extensions
 {
     public static class ControllerExtensions
     {
-        public static OkObjectResult CallWithoutTryCatch<TRequest, TResponse>(
+        public static OkObjectResult Call<TRequest, TResponse>(
             TRequest request,
+            IValidator<TRequest> _validator,
             Func<TRequest, TResponse> function,
-            string successMessage
+            string successMessage = "Successfully processed request."
         )
-            => new OkObjectResult(new ApiResponse<TResponse>()
+        {
+
+            return new OkObjectResult(new ApiResponse<TResponse>()
             {
                 Data = function(request),
                 SuccessMessage = successMessage,
                 Success = false
             });
+        }
+
     }
 }
