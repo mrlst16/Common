0b221d8f8545c5fd94f0a8e48a91ed152b578250
matt <mrlst16@mail.rmu.edu>
2023-05-11 09:17:22 -0700

    add get row and get column

--- Common.Extensions.Test/ArrayExtensionsTests.cs
+++ Common.Extensions.Test/ArrayExtensionsTests.cs
@@ -5,22 +5,49 @@ namespace Common.Extensions.Test
     public class ArrayExtensionsTests
     {
         #region Tests
-        
+
         [Theory]
         [MemberData(nameof(SwitchTestData))]
         public void SwitchTheory(int p1, int p2, int[] expected)
         {
             int[] array = { 1, 2, 3, 4 };
             var result = array.Switch(p1, p2);
-            
-            Assert.Equal(result, expected);
+
+            Assert.Equal(expected, result);
         }
 
         [Theory]
-        [MemberData(nameof(UniqueTestData))]
+        [MemberData(nameof(IsUniqueTestData))]
         public void IsUniqueTheory(int[] array, bool expected)
         {
             var result = array.IsUnique();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetRowTestData))]
+        public void GetRowTheory(int row, int[] expected)
+        {
+            int[,] array = new int[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 }
+            };
+            var result = array.GetRow(row);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetColumnTestData))]
+        public void GetColumnTheory(int column, int[] expected)
+        {
+            int[,] array = new int[,] {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 }
+            };
+            var result = array.GetColumn(column);
             Assert.Equal(expected, result);
         }
 
@@ -36,10 +63,23 @@ namespace Common.Extensions.Test
             new object[]{ 2, 3, new int[] { 1, 2, 4, 3 } }
         };
 
-        private static IEnumerable<object[]> UniqueTestData() => new List<object[]>
+        private static IEnumerable<object[]> IsUniqueTestData() => new List<object[]>
         {
             new object[]{ new int[] {1, 2, 3, 4, 5, 6, 7}, true },
             new object[]{ new int[] {1, 2, 3, 4, 5, 6, 7, 1}, false }
+        };
+
+        private static IEnumerable<object[]> GetRowTestData() => new List<object[]>
+        {
+            new object[]{ 0, new int[]{ 1, 2} },
+            new object[]{ 1, new int[]{ 3, 4} },
+            new object[]{ 2, new int[]{ 5, 6} }
+        };
+
+        private static IEnumerable<object[]> GetColumnTestData() => new List<object[]>
+        {
+            new object[]{ 0, new int[]{ 1, 3, 5} },
+            new object[]{ 1, new int[]{ 2, 4, 6} }
         };
 
         #endregion
--- Common.Extensions/ArrayExtensions.cs
+++ Common.Extensions/ArrayExtensions.cs
@@ -16,5 +16,26 @@
                 .Select(x => x.Count())
                 .Any(x => x > 1);
 
+        public static T[] GetRow<T>(this T[,] array, int rowNumber)
+        {
+            int length = array.GetLength(1);
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = array[rowNumber, i];
+            }
+            return result;
+        }
+
+        public static T[] GetColumn<T>(this T[,] array, int columnNumber)
+        {
+            int length = array.GetLength(0);
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = array[i, columnNumber];
+            }
+            return result;
+        }
     }
 }
