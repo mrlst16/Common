546e16aaeaab189497648fee841e03a3b32b6e39
matt <mrlst16@mail.rmu.edu>
2023-05-10 23:24:01 -0700

    add array extensions

---
+++ Common.Extensions.Test/ArrayExtensionsTests.cs
@@ -0,0 +1,47 @@
+using Common.Extensions;
+
+namespace Common.Extensions.Test
+{
+    public class ArrayExtensionsTests
+    {
+        #region Tests
+        
+        [Theory]
+        [MemberData(nameof(SwitchTestData))]
+        public void SwitchTest(int p1, int p2, int[] expected)
+        {
+            int[] array = { 1, 2, 3, 4 };
+            var result = array.Switch(p1, p2);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(UniqueTestData))]
+        public void IsUniqueTests(int[] array, bool expected)
+        {
+            var result = array.IsUnique();
+            Assert.Equal(expected, result);
+        }
+
+        #endregion
+
+
+        #region Member Data
+
+        private static IEnumerable<object[]> SwitchTestData() => new List<object[]>
+        {
+            new object[]{ 0, 1, new int[]{ 2, 1, 3, 4 } },
+            new object[]{ 1, 2, new int[]{ 1, 3, 2, 4} },
+            new object[]{ 2, 3, new int[] { 1, 2, 4, 3 } }
+        };
+
+        private static IEnumerable<object[]> UniqueTestData() => new List<object[]>
+        {
+            new object[]{ new int[] {1, 2, 3, 4, 5, 6, 7}, true },
+            new object[]{ new int[] {1, 2, 3, 4, 5, 6, 7, 1}, false }
+        };
+
+        #endregion
+    }
+}
---
+++ Common.Extensions/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Switch<T>(this T[] array, int p1, int p2)
+        {
+            var p1Val = array[p1];
+            var p2Val = array[p2];
+            array[p1] = p2Val;
+            array[p2] = p1Val;
+            return array;
+        }
+
+        public static bool IsUnique<T>(this T[] array) =>
+            !array.GroupBy(k => k)
+                .Select(x => x.Count())
+                .Any(x => x > 1);
+    }
+}
